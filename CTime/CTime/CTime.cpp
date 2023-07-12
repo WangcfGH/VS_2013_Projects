7f312f88f0abf508172f90b5b4b631a76e1e637d
WangcfBB <15988132243@163.com>
2023-07-12 11:24:00 +0800

    CTime有改动

--- CTime/CTime/CTime.cpp
+++ CTime/CTime/CTime.cpp
@@ -17,6 +17,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	CTimeSpan tSpanTime = tNow - tStamp;
 	int nSeconds = tSpanTime.GetSeconds();
 
+	DWORD dwValue1 = 1412296875;
+	DWORD dwValue2 = 1412297765;
+	DWORD dwValue3 = 151000;
+
+	if (dwValue1 - dwValue2 > dwValue3)
+	{
+		std::cout << "Bigger" << endl;
+	} 
+	else
+	{
+		std::cout << "Smaller" << endl;
+	}
+
 	return 0;
 }
 
